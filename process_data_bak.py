d9baad2e168dac0dd1d5e8621491fdbbae219ff4
oxygensu <835241707@qq.com>
2020-04-02 15:41:50 +0800

    first time to submit kaggle

--- process_data_bak.py
+++ process_data_bak.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn import linear_model
-import sklearn.preprocessing as preprocessing
 
+# read train.csv
+train_data = pd.read_csv('data/train.csv')
 
 def fill_age(df):
     # 把已有的数值型特征取出来丢进Random Forest Regressor中
@@ -38,6 +38,58 @@ def set_Cabin_type(df):
     return df
 
 
+print("0:\n")
+print(train_data.describe())
+
+train_data, rfr = fill_age(train_data)
+train_data = set_Cabin_type(train_data)
+print("1:\n")
+print(train_data.describe())
+
+
+dummies_Cabin = pd.get_dummies(train_data['Cabin'], prefix= 'Cabin')
+dummies_Embarked = pd.get_dummies(train_data['Embarked'], prefix= 'Embarked')
+dummies_Sex = pd.get_dummies(train_data['Sex'], prefix= 'Sex')
+dummies_Pclass = pd.get_dummies(train_data['Pclass'], prefix= 'Pclass')
+df = pd.concat([train_data, dummies_Cabin, dummies_Embarked, dummies_Sex, dummies_Pclass], axis=1)
+df.drop(['Pclass', 'Name', 'Sex', 'Ticket', 'Cabin', 'Embarked'], axis=1, inplace=True)
+print("2:\n")
+print(df.describe())
+
+
+import sklearn.preprocessing as preprocessing
+scaler = preprocessing.StandardScaler()
+
+age_reshaped = df['Age'].values.reshape(-1, 1)
+age_scale_param = scaler.fit(age_reshaped)
+df['Age_scaled'] = scaler.fit_transform(age_reshaped, age_scale_param)
+
+fare_reshaped = df['Fare'].values.reshape(-1, 1)
+fare_scale_param = scaler.fit(fare_reshaped)
+df['Fare_scaled'] = scaler.fit_transform(fare_reshaped, fare_scale_param)
+print("3:\n")
+print(df.describe())
+
+
+# train a model
+from sklearn import linear_model
+
+# 用正则取出我们要的属性值
+train_df = df.filter(regex='Survived|Age_.*|SibSp|Parch|Fare_.*|Cabin_.*|Embarked_.*|Sex_.*|Pclass_.*')
+train_np = train_df.values
+
+# y即Survival结果
+y = train_np[:, 0]
+
+# X即特征属性值
+X = train_np[:, 1:]
+
+# fit到RandomForestRegressor之中
+clf = linear_model.LogisticRegression(C=1.0, penalty='l2', tol=1e-6)
+clf.fit(X, y)
+print('model:\n')
+print(clf)
+
 def preprocessing_data(train_data):
     train_data, rfr = fill_age(train_data)
     train_data = set_Cabin_type(train_data)
@@ -66,4 +118,4 @@ def preprocessing_data(train_data):
     # fit到RandomForestRegressor之中
     clf = linear_model.LogisticRegression(C=1.0, penalty='l2', tol=1e-6)
     clf.fit(X, y)
-    return train_np, clf
+    return clf
