ba8be9b705e54941bfcbc4b33a3c63d3bf5d36a3
oxygensu <835241707@qq.com>
2020-04-07 12:15:19 +0800

    1

---
+++ calculate_acc.py
@@ -0,0 +1,22 @@
+'''
+Used to calculate the accuracy of the predictions
+'''
+import numpy as np
+import pandas as pd
+
+# load predictions csv
+predictions = pd.read_csv('data/tf_predictions_adam3.csv')
+# load answer csv
+answer = pd.read_csv('data/gender_submission.csv')
+
+correct = 0
+not_correct = 0
+
+for each in predictions:
+    # survived = each['PassengerId'].values
+    # if each['Survived'] == (answer[each['PassengerId']]):
+    #     correct = correct + 1
+    # else:
+    not_correct = not_correct + 1
+
+print("acc = %.3f", (correct/(correct+not_correct)))
