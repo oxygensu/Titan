2c06ce44b977efc7e195020f238755aa41310f67
oxygensu <835241707@qq.com>
2020-04-09 16:23:38 +0800

    acc

--- calculate_acc.py
+++ calculate_acc.py
@@ -3,22 +3,26 @@ Used to calculate the accuracy of the predictions
 '''
 import numpy as np
 import pandas as pd
-
+import sklearn
 # load predictions csv
-predictions = pd.read_csv('data/tf_predictions_adam3.csv')
+predictions = pd.read_csv('data/tf_predictions.csv')
 # load answer csv
-answer = pd.read_csv('data/gender_submission.csv')
+answer = pd.read_csv('data/submission.csv')
+
+
+predictions_np = predictions.values
+answer_np = answer.values
 
 correct = 0
 not_correct = 0
 
-survived = predictions.to_dict(orient='PassengerId')
-
-# for each in predictions:
-    # survived = each['PassengerId'].values
-    # if each['Survived'] == (answer[each['PassengerId']]):
-    #     correct = correct + 1
-    # else:
-    # not_correct = not_correct + 1
-
-print("acc = %.3f", (correct/(correct+not_correct)))
+for idx in range(len(predictions_np)):
+    a = predictions_np[idx][1]
+    b = answer_np[idx][1]
+    if (a == b):
+        correct = correct + 1
+    else:
+        not_correct = not_correct + 1
+        
+    
+print("%.5f"%(correct/(not_correct+correct)))
